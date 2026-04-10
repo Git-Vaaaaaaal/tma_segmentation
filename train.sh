95a1de48ad9988ed7e5419f428f2c4d06f3a1b89
Valentin Bosch <val.bosch2001@gmail.com>
2026-04-10 11:18:04 +0200

    up

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 #$ -N clamv2
 cd $WORKDIR
 cd /beegfs/data/work/c-2iia/vb710264/tma_segmentation
-source /beegfs/data/work/c-2iia/vb710264/tma_segmentation/tma_segmentation_venv/venv/bin/activate
+source /beegfs/data/work/c-2iia/vb710264/tma_segmentation/venv/bin/activate
 module load python
 export PYTHONPATH=/work/c-2iia/vb710264/tma_segmentation/venv/lib/python3.9/site-packages:$PYTHONPATH
 export MPLCONFIGDIR=/work/c-2iia/vb710264/.cache/matplotlib
